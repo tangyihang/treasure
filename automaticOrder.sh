243ca05e89fdd88819310a74ec303df7798d8316
tangyihang <tangyihangchina@163.com>
2019-02-27 16:38:05 +0800

    定时任务修改

--- automaticOrder.sh
+++ automaticOrder.sh
@@ -1,4 +1,4 @@
-server=`ps -aux | grep /www/wwwroot/treasure/cli.php admin/automaticOrder | grep -v grep`
+server=`ps -aux | grep '/www/wwwroot/treasure/cli.php admin/automaticOrder' | grep -v grep`
         if [ ! "$server" ]; then
                 nohup php /www/wwwroot/treasure/cli.php admin/automaticOrder &
                 echo "restart process success"
--- kj.sh
+++ kj.sh
@@ -1,4 +1,4 @@
-server=`ps -aux | grep /www/wwwroot/treasure/cli.php admin/award | grep -v grep`
+server=`ps -aux | grep '/www/wwwroot/treasure/cli.php admin/award' | grep -v grep`
         if [ ! "$server" ]; then
                 nohup php /www/wwwroot/treasure/cli.php admin/award &
                 echo "restart process success"
